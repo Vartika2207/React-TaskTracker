85b5b937e2b9b62db26311894ce99ea2928618fc
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-02-01 09:00:41 +0530

    Added react scripts
    Added get and post request, added custome hoo

---
+++ 02-Building-and-using-custome-hook/src/App.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transformTasks = taskObj => {
+    const loadedTasks = [];
+  
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+
+  const httpData = useHttp({url: 'https://react-http-43b44-default-rtdb.firebaseio.com/tasks.json'},
+           transformTasks        
+                          );
+
+  // giving an alias to sendRequest 
+  const {isLoading, error, sendRequest: fetchTasks} = httpData;                       
+
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       'https://react-http-43b44-default-rtdb.firebaseio.com/tasks.json'
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error('Request failed!');
+  //     }
+
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || 'Something went wrong!');
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ 02-Building-and-using-custome-hook/src/hooks/use-http.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+const useHttp = (requestConfig, applyData) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
+    const sendRequest = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          requestConfig.url, 
+          {
+            method: requestConfig.method ?  requestConfig.method : 'GET',
+            headers: requestConfig.headers ? requestConfig.headers : {},
+            body: requestConfig ? JSON.stringify(requestConfig.body) : null
+          }
+        );
+  
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+  
+        const data = await response.json();
+        applyData(data);
+  
+      } catch (err) {
+        setError(err.message || 'Something went wrong!');
+      }
+      setIsLoading(false);
+    };
+    return {
+        isLoading: isLoading,
+        error: error,
+        sendRequest: sendRequest
+        // we can also use a modern JavaScript shortcut here
+        // isLoading,
+        // error,
+        // sendRequest
+    }
+};
+
+export default useHttp;
