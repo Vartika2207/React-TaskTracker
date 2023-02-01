358cf942b14b0bf856563f673221f906c08ebe7d
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-02-01 09:20:26 +0530

    Update App.js

--- 02-Building-and-using-custome-hook/src/App.js
+++ 02-Building-and-using-custome-hook/src/App.js
@@ -23,33 +23,6 @@ function App() {
   // giving an alias to sendRequest 
   const {isLoading, error, sendRequest: fetchTasks} = httpData;                       
 
-  // const fetchTasks = async (taskText) => {
-  //   setIsLoading(true);
-  //   setError(null);
-  //   try {
-  //     const response = await fetch(
-  //       'https://react-http-43b44-default-rtdb.firebaseio.com/tasks.json'
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Error('Request failed!');
-  //     }
-
-  //     const data = await response.json();
-
-  //     const loadedTasks = [];
-
-  //     for (const taskKey in data) {
-  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-  //     }
-
-  //     setTasks(loadedTasks);
-  //   } catch (err) {
-  //     setError(err.message || 'Something went wrong!');
-  //   }
-  //   setIsLoading(false);
-  // };
-
   useEffect(() => {
     fetchTasks();
   }, []);
